d9be504b4be34cb05babf935acd430f2f3ef4f50
Alex <last0220@gmail.com>
2020-09-18 18:46:19 +0900

    fix: bug

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const createError = require('http-errors');
 const express = require('express');
+const session = require('express-session')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -7,14 +7,11 @@ const config = require('./config/config');
 const tokenAbi = require('./config/erc20ABI')
 const request = require('request');
 
-
 // init web3
 const Web3 = require('web3');
-const web3 = new Web3(config.getConfig().httpEndpoint);
+const web3 = new Web3(config.getConfig().httpEndpoint + '/v1/ETH/rpc');
 
 let app = express();
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -24,29 +21,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-let token_list = []
-let address
+let token_list = [];
+let address;
 
-app.use('/login', function(req, res) {
-  var method = req.method;
-
-  if (method == 'GET') {
-    res.render('login');
-  } else {
-    let privateKey = req.param('private_key');
-    address = req.param('address');
-    let globalConfig = config.getConfig();
-    globalConfig.privateKey = privateKey;
-    globalConfig.address = address;
-
-    res.redirect('/');
-  }
-});
-
+/*
+  Default Route
+*/
 app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
+app.post('/login', function(req, res) {
+  const address = req.param('address');
+  const privateKey = req.param('private_key');
+  let globalConfig = config.getConfig();
+  globalConfig.privateKey = privateKey;
+  globalConfig.address = address;
+  res.redirect('/');
+});
 
 app.get('/api/get_info', async function(req, res) {
   const address = config.getConfig().address;
@@ -55,25 +51,29 @@ app.get('/api/get_info', async function(req, res) {
   res.json( { balance: ether, address: address});
 });
 
-
 app.post('/api/transfer', async function(req, res) {
   let fromAddress = config.getConfig().address;
   let privateKey = config.getConfig().privateKey;
   let contractAddress = req.param('contract');
   let toAddress = req.param('to_address');
   let amount = req.param('amount');
-  let etherToWei = web3.utils.toWei(amount, 'ether');
+  let etherToWei = web3.utils.toWei(amount.toString(), 'ether');
 
-  let rawTx = {}
-  let nonce = web3.eth.getTransactionCount(fromAddress, 'pending');
-  if(contractAddress) {
+  let rawTx = {};
+  let nonce = await web3.eth.getTransactionCount(fromAddress, 'pending');
+  // let gasPrice = web3.utils.toHex(web3.utils.toWei('100', 'gwei'));
+  const gasPrice = 30000000000;
+  const gasLimit = 2000000;
+
+  if (contractAddress) {
     let tokenContract = new web3.eth.Contract(tokenAbi, contractAddress);
     let inputData = tokenContract.methods.transfer(toAddress, amount).encodeABI();
+
     rawTx = {
       to: contractAddress,
       value: 0,
-      gasPrice: '30000000000',
-      gas: '210000',      
+      gasPrice,
+      gas: '200000',
       data: inputData,
       nonce: nonce
     };
@@ -82,66 +82,56 @@ app.post('/api/transfer', async function(req, res) {
       from: fromAddress,
       to: toAddress,
       value: etherToWei,
-      gasPrice: '30000000000',
-      gas: '21000',
+      gasPrice,
+      gasLimit,
       nonce: nonce
     };
   }
-    
-  let account = web3.eth.accounts.privateKeyToAccount(privateKey)
-  let signedTx = await account.signTransaction(rawTx)
 
-  let data = {}
+  if (privateKey.startsWith('0x')) {
+    privateKey = privateKey.replace('0x', '');
+  }
+
+  let account = web3.eth.accounts.privateKeyToAccount(privateKey);
+  let signedTx = await account.signTransaction(rawTx);
+  let data = {};
 
   let txInfo = await web3.eth.sendSignedTransaction(signedTx.rawTransaction, (err, txHash) => {
     if (err) {
-      console.log('========== transaction 발생 중 에러 ===========')
-      data.result = 'fail'
-      res.json(data)
-      return
+      data.result = 'fail';
+      res.json(data);
+      return;
     }
-    console.log('========== transaction 발생 ===========')
-  })
-  console.log('========== transaction 처리완료 ===========')
-  data.result = 'success'
-  data.txInfo = txInfo
-  res.json(data)
+  });
+  data.result = 'success';
+  data.txInfo = txInfo;
+  res.json(data);
 })
 
 
 app.get('/api/get_history', async function(req, res) {
-  var my_address = config.getConfig().address;
-  console.log(my_address)
-
-  let options = {
-    uri: "http://api-ropsten.etherscan.io/api",
-    qs: {
-      module: "account",
-      action: "txlist",
-      address: my_address,
-      startblock: 0,
-      endblock: 99999999,
-      sort: "asc",
-      apikey: config.getConfig().etherscan_api_key
-    }
+  const address = config.getConfig().address;
+  const httpEndpoint = config.getConfig().httpEndpoint;
+  const options = {
+    uri: `${httpEndpoint}/v1/ETH/addresses/${address}/transactions`,
   }
 
   request(options, (error, response, result) => {
-    if(error) {
-      console.log(error);
-    } else {
-      res.json(JSON.parse(result))
+    let txs = JSON.parse(result);
+    for (let i = 0; i < txs.length; i++) {
+      txs[i].value = web3.utils.fromWei(txs[i].value.toString(), 'ether');
     }
-  })
+
+    res.json(txs);
+  });
 })
 
 app.get('/api/get_token', async function(req, res) {
-  var data = {}
-  for(var i = 0; i < token_list.length; i++) {
-    console.log(token_list[i])
-    var token_Contract = new web3.eth.Contract(tokenAbi, token_list[i]);
-    var token_symbol = await token_Contract.methods.symbol().call();
-    var token_balance = await token_Contract.methods.balanceOf(address).call();
+  let data = {}
+  for (let i = 0; i < token_list.length; i++) {
+    const token_Contract = new web3.eth.Contract(tokenAbi, token_list[i]);
+    const token_symbol = await token_Contract.methods.symbol().call();
+    const token_balance = await token_Contract.methods.balanceOf(address).call();
     data[i] = {
       symbol: token_symbol,
       balance: token_balance
@@ -151,9 +141,8 @@ app.get('/api/get_token', async function(req, res) {
 })
 
 app.post('/api/add_token', async function(req, res) {
-  var contract_Address = req.param('token_contract');
-  token_list.push(contract_Address)
+  const contractAddress = req.param('token_contract');
+  token_list.push(contractAddress);
 })
-
 
 module.exports = app;
--- config/config.js
+++ config/config.js
@@ -1,11 +1,12 @@
 const config = {
-    address: '',
-    privateKey: '',
-    httpEndpoint: 'https://ropsten.infura.io/v3/1dd42478d4e64ff4b172ae5779e1ceb9',
-    etherscan_api_key: "4FCWFXMHFGXV4J525NWS6235598M7JBGBX"
+  // address: '0xD3590B15695f491CB1E8a56F6439C20DFB76f787',
+  // privateKey: '0x003D60DF3D60CA4321E8FF7BAE8C2B9F2821D9FF826AB49DFFF74B5E47AA3EDE',
+  address: '',
+  privateKey: '',
+  httpEndpoint: 'http://octet-fullhistory-ropsten.hexlant.com:2020',
+  accessKey: '4FCWFXMHFGXV4J525NWS6235598M7JBGBX'
 }
 
-
 exports.getConfig = function () {
-    return config;
+  return config;
 }
